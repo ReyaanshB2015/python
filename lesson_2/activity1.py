5ce41cea6e90be2d2c41b13da44f238cc997453a
ReyaanshB2015 <saheli83@gmail.com>
2025-06-29 12:27:55 +0530

    Add files via upload

---
+++ lesson_2/activity1.py
@@ -0,0 +1,4 @@
+print("hello world!")
+print("i study in grade", 5)
+print("good job", end= '!!!!!!!')
+print("\nthe sum of two numbers is",69+420)
---
+++ lesson_2/activity2.py
@@ -0,0 +1,6 @@
+num1=4200
+num2=69
+diffrence= num1-num2
+print (diffrence)
+name="Reyaansh Bandyopadhaya"
+print(name)
