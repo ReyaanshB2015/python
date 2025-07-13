66f847c14ec92dc13d49ccb56c53ff13e7800070
ReyaanshB2015 <saheli83@gmail.com>
2025-07-13 12:25:09 +0530

    Add files via upload

---
+++ lesson_6/activity1.py
@@ -0,0 +1,16 @@
+a=10
+g=10
+n=0
+if a>0 and g>0 and n==0:
+    print("all of the numbers have boolean value as true")
+else:
+    print("one of the numbers have boolean value as false")
+
+#or oprerator example
+b=20
+c=30
+m=-20
+if b>0 or c>0 or m==-20:
+    print("any one of the numbers have boolean value as true")
+else:
+    print("all of the numbers have boolean value as true")
