8e2659dcab3600c2c5b5c4193ce4f6935def98c0
ReyaanshB2015 <saheli83@gmail.com>
2025-07-13 21:57:40 +0530

    Add files via upload

---
+++ lesson_6/hw1.py
@@ -0,0 +1,56 @@
+a=input("enter ur alaphabet")
+a=a.lower()
+if a=="a":
+    print("the letter is apart of the alphabet")
+elif a=="b":
+    print("the letter is apart of the alphabet")
+elif a=="c":
+    print("the letter is apart of the alphabet")
+elif a=="d":
+    print("the letter is apart of the alphabet")
+elif a=="e":
+    print("the letter is apart of the alphabet")
+elif a=="f":
+    print("the letter is apart of the alphabet")
+elif a=="g":
+    print("the letter is apart of the alphabet")
+elif a=="h":
+    print("the letter is apart of the alphabet")
+elif a=="i":
+    print("the letter is apart of the alphabet")
+elif a=="j":
+    print("the letter is apart of the alphabet")
+elif a=="k":
+    print("the letter is apart of the alphabet")
+elif a=="l":
+    print("the letter is apart of the alphabet")
+elif a=="m":
+    print("the letter is apart of the alphabet")
+elif a=="n":
+    print("the letter is apart of the alphabet")
+elif a=="o":
+    print("the letter is apart of the alphabet")
+elif a=="p":
+    print("the letter is apart of the alphabet")
+elif a=="q":
+    print("the letter is apart of the alphabet")
+elif a=="r":
+    print("the letter is apart of the alphabet")
+elif a=="s":
+    print("the letter is apart of the alphabet")
+elif a=="t":
+    print("the letter is apart of the alphabet")
+elif a=="u":
+    print("the letter is apart of the alphabet")
+elif a=="v":
+    print("the letter is apart of the alphabet")
+elif a=="w":
+    print("the letter is apart of the alphabet")
+elif a=="x":
+    print("the letter is apart of the alphabet")
+elif a=="y":
+    print("the letter is apart of the alphabet")
+elif a=="z":
+    print("the letter is apart of the alphabet")
+else:
+    print("it is NOT apart of the alphabet")
