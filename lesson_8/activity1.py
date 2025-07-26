f112cde8b8d9e6371ae298ccfd0856ce12427198
ReyaanshB2015 <saheli83@gmail.com>
2025-07-26 12:22:42 +0530

    Add files via upload

---
+++ lesson_8/activity1.py
@@ -0,0 +1,13 @@
+a=5
+b=1
+c=2
+d=3
+e=a*b+(a//c)+d+b
+print(e)
+
+name="Reyaansh"
+age=10
+if name=="Reyaansh" or name=="Jhon" and age >=9:
+    print("hello there sir")
+else:
+    print("goodbye!")
---
+++ lesson_8/activity2.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+if a%b==0:
+    print("the number is divisible")
+else:
+    print("the number is not divisible")
