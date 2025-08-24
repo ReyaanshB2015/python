5447b87e232163e263d8a3eca2a74ff5c0517f7b
ReyaanshB2015 <saheli83@gmail.com>
2025-08-24 12:23:44 +0530

    Add files via upload

---
+++ lesson_17/activity.py
@@ -0,0 +1,7 @@
+a=input("enter ur word ")
+for c in a:
+    if c=="a":
+        print("A is found")
+        break
+    else:
+        print("A is not found")
---
+++ lesson_17/activity2.py
@@ -0,0 +1,7 @@
+var=10
+while var>0:
+    var=var-1
+    if var==5:
+        continue
+    print("the current value is ",var)
+print("good bye!")
