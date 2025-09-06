51ad990fdc66f1d822ab60e8924e13b6201f929c
ReyaanshB2015 <saheli83@gmail.com>
2025-09-06 12:27:28 +0530

    Add files via upload

---
+++ lesson_20/activity1.py
@@ -0,0 +1,7 @@
+
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print(today)
+print(now)
+print("date components are ",today.year,today.month,today.day)
---
+++ lesson_20/activity2.py
@@ -0,0 +1,4 @@
+import calendar
+yr=2025
+mn=10
+print(calendar.month(yr,mn))
